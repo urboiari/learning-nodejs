dc16249d9165a26cdcb0dbdbef6706003c74d9a1
Ari Rivera <soewarsoari@gmail.com>
2020-08-25 12:20:00 -0700

    Receive POST from client and return it

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ const server = express();
 
 server.listen(8000, () => console.log('listening at 8000'));
 server.use(express.static('public'));
+server.use(express.json({limit: '1mb'}));
+
+server.post('/api', (request, response) => {
+  let data = request.body;
+  console.log(data);
+  response.json({
+    status: 'success',
+    latitude: data.lat,
+    longitude: data.lon
+  });
+});
